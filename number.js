520b50ab35667d1c9b2507f9d387b384c14763c0
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-13 23:44:57 -0400

    first commit. number amd math in Javascript.

---
+++ number.js
@@ -0,0 +1,49 @@
+//Compound Assignment With Augmented Addition
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a+=12;
+b+=9;
+c+=7;
+
+//Compound Assignment With Augmented Subtraction
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a-=6;
+b-=15;
+c-=1;
+
+
+//Compound Assignment With Augmented Multiplication
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a*=5;
+b*=b;
+c*=10;
+
+
+//Compound Assignment With Augmented Division
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a/=12;
+b/=4;
+c/=11;
