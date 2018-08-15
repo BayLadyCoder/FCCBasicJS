f9c053494a0e0b5e895d0a050527ab2b25aef30c
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-14 21:59:00 -0400

    string 2

--- string.js
+++ string.js
@@ -51,3 +51,81 @@ var myStr = "Learning to code is ";
 
 myStr += someAdjective;
 
+
+// Find the Length of a String
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+lastNameLength = lastName.length;
+
+
+// Use Bracket Notation to Find the First Character in a String
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
+
+
+//Use Bracket Notation to Find the Nth Character in a String
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
+
+
+//Use Bracket Notation to Find the Last Character in a String
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length-1];
+
+
+//Use Bracket Notation to Find the Nth-to-Last Character in a String
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+
+// Word Blanks
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "My " + myNoun + " is " + myAdjective + ", and he " + myVerb + " so " + myAdverb;
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
