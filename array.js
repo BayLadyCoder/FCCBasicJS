23d64c107a307d3ca0cdaa999a39e0a414689e8d
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-14 22:11:35 -0400

    array

---
+++ array.js
@@ -0,0 +1,120 @@
+// Store Multiple Values in one Variable using JavaScript Arrays
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Bay", 28];
+
+
+//Nest one Array within Another Array
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Sammy", 9], ["Katniss", 3]];
+
+
+//Access Array Data with Indexes
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
+
+
+//Modify Array Data With Indexes
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;
+
+
+//Access Multi-Dimensional Arrays With Indexes
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
+
+/*
+arr[3]; // equals [[10,11,12], 13, 14]
+arr[3][0]; // equals [10,11,12]
+arr[3][0][1]; // equals 11
+*/
+
+
+//Manipulate Arrays With push()
+var arr = [1,2,3];
+arr.push(4);
+// arr is now [1,2,3,4]
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]); 
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3]);
+
+
+//Manipulate Arrays With pop()
+var threeArr = [1, 4, 6];
+var oneDown = threeArr.pop();
+console.log(oneDown); // Returns 6
+console.log(threeArr); // Returns [1, 4]
+
+// Example
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop(); 
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
+
+
+//Manipulate Arrays With shift()
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
+
+
+//Manipulate Arrays With unshift()
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy"); 
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul", 35]);
+
+
+
+//Shopping List
+var myList = [["Corn", 5], ["Orange Juice", 2], ["Soy Milk", 1], ["Bread", 2], ["Broccoli", 1]];
