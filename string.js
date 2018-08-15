90dec7e7c10c463ba519a6d3a37b803b60647d7b
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-14 21:34:54 -0400

    string 1

---
+++ string.js
@@ -0,0 +1,53 @@
+
+// Escaping Literal Quotes in Strings
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+
+// Quoting Strings with Single Quotes
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+// Escape Sequences in Strings
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
+// Concatenating Strings with Plus Operator
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+var myStr = "This is the start. " + "This is the end.";
+
+
+// Concatenating Strings with the Plus Equals Operator
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+var myStr = "This is the first sentence. ";
+
+myStr += "This is the second sentence.";
+
+
+// Constructing Strings with Variables
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Bay";
+var myStr = "My name is " + myName + " and I am well!"; 
+
+// Appending Variables to Strings
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+var someAdjective = "fun!";
+var myStr = "Learning to code is ";
+
+myStr += someAdjective;
+
