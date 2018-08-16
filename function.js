701b0d9ecbbedd3c25deb4e67a803a4493e85ae4
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-16 19:38:51 -0400

    assignment with a return value.

--- function.js
+++ function.js
@@ -150,3 +150,23 @@ function addFive(num) {
 
 // Only change code above this line
 var returnedValue = addFive(5);
+
+//Assignment with a Returned Value
+// Example
+var changed = 0;
+
+function change(num) {
+  return (num + 5) / 3;
+}
+
+changed = change(10);
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+var processed = processArg(7);
