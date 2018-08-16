bd64adb8cb259f98e8fbb4ac7d3b255d16c369e6
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-16 19:49:20 -0400

    Stand in Line exercise for array and functions in Javascript.

--- function.js
+++ function.js
@@ -170,3 +170,19 @@ function processArg(num) {
 
 // Only change code below this line
 var processed = processArg(7);
+
+
+//Stand in Line
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  return arr.shift();  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
