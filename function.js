198c652a615157ec55869d1ba75537ac4774a7f4
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-15 23:20:28 -0400

    function - Local Scope in Functions

--- function.js
+++ function.js
@@ -55,4 +55,17 @@ function fun2() {
 	it's declared inside a function block.
 */
 
-//
+
+// Local Scope and Functions
+function myLocalScope() {
+  'use strict'; // you shouldn't need to edit this line
+  var myVar = "test";
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log(myVar);
+
+// Now remove the console log line to pass the test
