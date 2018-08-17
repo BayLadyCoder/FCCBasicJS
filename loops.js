095fb2d4ee5bed1f6a06b83718556cbabbdce366
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-17 13:43:09 -0400

    Do...While Loops

--- loops.js
+++ loops.js
@@ -77,3 +77,38 @@ var total = 0;
 for (var i = 0; i < myArr.length ; i++) {
   total += myArr[i];
 }
+
+
+//Nesting For Loops
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i =0 ; i < arr.length; i++) {
+    for (var j =0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  // Only change code above this line
+  return product;
+}
+
+// Modify values below to test your code
+multiplyAll([[1,2],[3,4],[5,6,7]]);
+
+
+//Iterate with JavaScript Do...While Loops
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+while (i < 5) {
+  myArray.push(i);
+  i++;
+}
+
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
