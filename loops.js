20e86939fbc47bc106dcabf068297229f4de1c34
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-17 13:07:35 -0400

    For Loops

--- loops.js
+++ loops.js
@@ -8,3 +8,19 @@ while (i < 5) {
   myArray.push(i);
   i++;
 }
+
+//Iterate with JavaScript For Loops
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i =0; i <5; i++) {
+    myArray.push(i+1);
+}
