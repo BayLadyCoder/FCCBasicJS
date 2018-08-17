a3f8c4e20ddb4a6fb9e7c062a308d15ff9751ba2
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-16 20:04:40 -0400

    Comparison with Equality Operater ==

--- conditionalLogic.js
+++ conditionalLogic.js
@@ -33,3 +33,16 @@ function trueOrFalse(wasThatTrue) {
 
 // Change this value to test
 trueOrFalse(false);
+
+
+//Comparison with the Equality Operator
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
