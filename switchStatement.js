3fed61d61bd0dd6847e97b076073184536b44759
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-16 22:13:56 -0400

    Counting Cards Exercise

--- switchStatement.js
+++ switchStatement.js
@@ -98,3 +98,119 @@ function sequentialSizes(val) {
 
 // Change this value to test
 sequentialSizes(1);
+
+
+//Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  
+  if (val === "bob") {
+    answer = "Marley";
+  } else if (val === 42) {
+    answer = "The Answer";
+  } else if (val === 1) {
+    answer = "There is no #1";
+  } else if (val === 99) {
+    answer = "Missed me by this much!";
+  } else if (val === 7) {
+    answer = "Ate Nine";
+  }
+
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+chainToSwitch(7);
+
+
+//Returning Boolean Values from Functions
+
+function isLess(a, b) {
+  // Fix this code
+ return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
+
+
+//Return Early Pattern for Functions
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  
+   if (a < 0 || b < 0) {
+     return undefined;
+   }
+  
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+// Change values below to test your code
+abTest(2,2);
+
+
+//Counting Cards Exercise
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  var answer = "";
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count = count;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+     if (count > 0) {
+      return count + " Bet";
+    }
+    else {
+      return count + " Hold";
+    }
+ 
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
