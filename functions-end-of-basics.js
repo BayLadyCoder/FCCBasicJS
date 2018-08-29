52359d4dd055d41d918d6fa07045ecaa513db2a4
Rachada Bay Chairangsaris <rachada_32@hotmail.com>
2018-08-28 23:13:15 -0400

    Generate Random Whole Numbers with JavaScript using Math.floor(); and Math.random()*num;

--- functions-end-of-basics.js
+++ functions-end-of-basics.js
@@ -7,3 +7,22 @@ function randomFraction() {
   return Math.random();
   // Only change code above this line.
 }
+
+
+
+
+// Generate Random Whole Numbers with JavaScript
+// Using Math.floor(); and Math.random();
+
+// Example
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+  var result = 0;
+  result = Math.floor(Math.random()*10);
+  return result;
+}
